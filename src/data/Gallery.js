1ce8fc54714e4f0d731795446f7d75c7dbb23b77
unknown <geletabirhanu555@gmsil.com>
2026-07-03 15:53:28 -0700

    deploy commit

--- src/data/Gallery.js
+++ src/data/Gallery.js
@@ -2,5 +2,7 @@ import hero from "../assets/images/hero.jpg";
 import room1 from "../assets/images/room1.jpg";
 import garden from "../assets/images/garden.jpg";
 import lake from "../assets/images/lake.jpg";
+import room2 from "../assets/images/SB_chair.jpg";
+import abiy_banner from "../assets/images/abiy_banner.jpg";
 
-export default [hero, room1, garden, lake];
+export default [abiy_banner,hero, room1, garden, lake,room1,room2];
