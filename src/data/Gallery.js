80f6eddc05b1e826f3d2b482fab5f33015cb2e3c
unknown <geletabirhanu555@gmsil.com>
2026-07-04 00:16:46 -0700

    final comit

--- src/data/Gallery.js
+++ src/data/Gallery.js
@@ -5,5 +5,5 @@ import lake from "../assets/images/SB_lamp.jpg";
 import room2 from "../assets/images/SB_chair.jpg";
 import abiy_banner from "../assets/images/abiy_banner.jpg";
 import room3 from "../assets/images/room3.jpg";
-import tv from "../assets/images/sb_smartTV.jpg";
+import tv from "../assets/images/SB_smartTV.jpg";
 export default [abiy_banner,hero, room1,tv, garden, lake,room1,room2,room3];
